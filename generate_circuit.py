d9f21c8d9ce7453f69868604c3337c3def01685a
Benjamin Villalonga Correa <benjaminvillalonga@gmail.com>
2018-07-04 19:08:40 -0700

    Copied generate_circuit.py

---
+++ generate_circuit.py
@@ -0,0 +1,14 @@
+"""Scrpit to generate random circuits according to Sergio Boixo's prescription.
+
+Parameters:
+  --L1: first lateral dimension of the circuit.
+  --L2: second lateral dimension of the circuit.
+  --depth: second lateral dimension of the circuit (greater than 1).
+  --seed: random seed to generate the circuit.
+  --outfile: output file for the circuit.
+
+Raises:
+  ValueError: if the depth is smaller than 2.
+
+
+"""
